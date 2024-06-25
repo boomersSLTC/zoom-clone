6c1d9bbde45c7fa0d58769b2b4b3791986bd0468
Sandive <121681097+boomersSLTC@users.noreply.github.com>
2024-06-25 08:46:21 +0530

    Update config.js

--- app/src/config.js
+++ app/src/config.js
@@ -17,7 +17,7 @@ function getIPv4() {
     return '0.0.0.0'; // Default to 0.0.0.0 if no external IPv4 address found
 }
 
-const IPv4 = '44.233.151.27';
+const IPv4 = getIPv4();
 
 const numWorkers = require('os').cpus().length;
 
